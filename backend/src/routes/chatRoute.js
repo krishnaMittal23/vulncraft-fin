ea62c85775e6821f725126b7328e8929435b8d23
Nikhil-Yadav15 <nikhil.2202@gmail.com>
2026-01-06 22:48:35 +0530

    syntax fixes

--- backend/src/routes/chatRoute.js
+++ backend/src/routes/chatRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const chatController = require("../controllers/chatCont");
 
-// POST /api/chat - Simple chatbot endpoint to test LLM
+
 router.post("/", chatController.chat);
 
 module.exports = router;
